f5eb27d72183c80a00113448c92cd5cdfa88b929
Abhinav Gautam <abhinav.nanhey@gmail.com>
2024-03-27 03:39:21 +0530

    trailing feed

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const os = require("os");
 const jks = require("jks-js");
 const fs = require("fs");
 const winston = require("winston");
-const LogstashTransport = require('winston3-logstash-transport');
+const LogstashTransport = require("winston3-logstash-transport");
 
 const { JKS_PASSWORD, LOGSTASH_HOST, LOGSTASH_PORT, KEYSTORE_ALIAS } =
   process.env;
@@ -14,15 +14,18 @@ const { cert, key } = keystore[KEYSTORE_ALIAS];
 
 const logger = winston.createLogger();
 
-logger.add(new LogstashTransport({
-  mode:"tcp",
-  port: LOGSTASH_PORT,
-  host: LOGSTASH_HOST,
-  sslEnable: true,
-  sslKeyContent: key,
-  sslCertContent: cert,
-  formatted:false
-}));
+logger.add(
+  new LogstashTransport({
+    mode: "tcp",
+    port: LOGSTASH_PORT,
+    host: LOGSTASH_HOST,
+    sslEnable: true,
+    sslKeyContent: key,
+    sslCertContent: cert,
+    formatted: false,
+    trailingLineFeed: true,
+  })
+);
 
 logger.on("error", (error) => {
   // Make the decission in here
@@ -34,9 +37,6 @@ pm2.launchBus(function (err, bus) {
 
   bus.on("log:out", function (log) {
     if (log.process.name !== "PM2-LOGSTASH") {
-      //console.log(log);
-      // console.log(log.process.name, log.data);
-      // Log to gelf
       var message = {
         timestamp: new Date(log.at).toISOString(),
         version: "1",
@@ -47,15 +47,11 @@ pm2.launchBus(function (err, bus) {
         message: log.data,
       };
       logger.info(message);
-      //gelf.emit("gelf.log", message);
     }
   });
 
   bus.on("log:err", function (log) {
     if (log.process.name !== "PM2-LOGSTASH") {
-      //console.log(log);
-      // console.error(log.process.name, log.data);
-      // Log to gelf
       var message = {
         timestamp: new Date(log.at).toISOString(),
         version: "1",
@@ -65,7 +61,7 @@ pm2.launchBus(function (err, bus) {
         user: hostname,
         message: log.data,
       };
-      logger.error( message);
+      logger.error(message);
     }
   });
 
